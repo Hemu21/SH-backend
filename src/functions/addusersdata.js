d2789d908d3744abb0b0c8df74e76722b7715e63
Hemanth kumar <rajva5727@gmail.com>
2023-09-19 18:25:37 +0530

    Update addusersdata.js

--- src/functions/addusersdata.js
+++ src/functions/addusersdata.js
@@ -4,7 +4,9 @@ const usersModel = require("../../models/usersModel")
 const addUsersData =async (ele)=>{
     const add = new usersModel()
     add.$set({user_id:ele.user_id,email:ele.email,password:ele.password,call_id:ele.call_id,_id:ele.user_id,phno:ele.phno})
-    await add.save()
+    const res = await add.save()
+    return res
+    
 }
 
 module.exports = addUsersData;
