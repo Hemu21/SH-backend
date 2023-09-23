ea8743f7a6cd7b8cfd907712a4e6bbd48c9257d7
Hemanth kumar <rajva5727@gmail.com>
2023-09-24 01:07:02 +0530

    Update updateuserdata.js

--- src/functions/updateuserdata.js
+++ src/functions/updateuserdata.js
@@ -4,15 +4,11 @@ const auth = require("../auth/auth.js")
 
 const updateUserData = async (req,res) => {
     const isAllow = auth(req,res)
-    if(!isAllow){
         const id = req.body.id
         const ele = req.body.data
         console.log(ele)
         const data = await userModel.findByIdAndUpdate(id,{$set:ele})
         res.send({"message":"user data updated"})
-    }else{
-        res.send({"message":"failed to update"})
-    }
 }
 
 module.exports = updateUserData;
